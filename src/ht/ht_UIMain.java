9bdd36d41eceefff68a3758910599f3a9e165856
Guy R <Guy.Roban@Gmail.Com>
2023-05-26 20:15:59 +0300

    add extras code
    need to adjust size of text labels, and extras prints

--- src/ht/ht_UIMain.java
+++ src/ht/ht_UIMain.java
@@ -1,9 +1,10 @@
 package ht;
 
 public class ht_UIMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ht_UI.createFrame();
-        ht_UI.ageUI();
+        int ageDiscount = ht_UI.ageUI();
+        int extrasPrice = ht_UI.extrasUI();
 
     }
 }
